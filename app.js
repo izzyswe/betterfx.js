7819c3919f41bca205b1b8383f8854cf43ea6ba2
izzyswe <ikj134@yahoo.com>
2026-02-12 23:59:48 -0500

    renamed doEvent

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const selected = (selctor, contxt = document) => {
 const selectedAll = (selctor, contxt = document) => {
   const elms = contxt.querySelectorAll(selctor);
   if(elms.length === 0) throw new Error("Missing/wrong Elements selected for " + selctor + ".\nLocation:"); return elms;  }
-const doEvent = (event, selected_elm, func) => selected_elm.addEventListener(event, func);
+const trigger = (event, selected_elm, func) => selected_elm.addEventListener(event, func);
 const logs = (output) => console.log(output)
 const addTxt = (elm, content) => elm.textContent = content;
 //Note: dot notation (elm.style.color) is for fixed property names, bracket notation (elm.style[prop]) is for dynamic property names.
@@ -12,7 +12,7 @@ const addTxt = (elm, content) => elm.textContent = content;
 const addCSS = (elm, propVal) => {
     if (elm === null) throw new Error("Missing/wrong element selected for " + selctor + ".\nLocation:");
     return Object.assign(elm.style, propVal);   }
-const loop = (start, symbol, end, body) => {
+const loop = (start, symbol, end, dir, body) => {
   switch(symbol){
     case ">":
       if(start < end) throw new Error(`${start} is less than ${end}. \nLocation:`);
@@ -46,7 +46,7 @@ const html = (tagName, body) => {
 };
 //SAMPLE CODE
 // const h1 = selected("div h1");
-// doEvent("click", h1, () => {
+// trigger("click", h1, () => {
 //   addTxt(h1, "hello")
 //   addCSS(h1, {
 //     color: "red",
